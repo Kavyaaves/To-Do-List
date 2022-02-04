12e8a2881674c886ca4373dacb546adaf66f5c59
Kavya <kavya.murali@techdome.net.in>
2022-02-05 02:22:55 +0530

    Updates

---
+++ precache-manifest.6c2d9439165866f3e9c2ab3e18c38419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244fb7ee4af45893be6ddbd597779dd5",
+    "url": "/To-Do-List-Mod1/index.html"
+  },
+  {
+    "revision": "f80b5c3cdede78d5d633",
+    "url": "/To-Do-List-Mod1/static/css/main.20912364.chunk.css"
+  },
+  {
+    "revision": "836a4f726c85ff01eb8f",
+    "url": "/To-Do-List-Mod1/static/js/2.11d43a1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List-Mod1/static/js/2.11d43a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80b5c3cdede78d5d633",
+    "url": "/To-Do-List-Mod1/static/js/main.e08bf400.chunk.js"
+  },
+  {
+    "revision": "3a4fdac779bddda3307c",
+    "url": "/To-Do-List-Mod1/static/js/runtime-main.37e65f68.js"
+  }
+]);
